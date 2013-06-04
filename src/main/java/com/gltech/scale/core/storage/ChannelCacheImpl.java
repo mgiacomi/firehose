4ea53c4b226d5f08f0f11937ca669d09e3a95257
Matt Giacomini <matt@spark6.com>
2013-06-04 06:41:57 -0700

    Issue #10: 500 error if trying to create channel that already exists.

--- src/main/java/com/gltech/scale/core/storage/ChannelCacheImpl.java
+++ src/main/java/com/gltech/scale/core/storage/ChannelCacheImpl.java
@@ -2,12 +2,15 @@ package com.gltech.scale.core.storage;
 
 import com.gltech.scale.core.model.ChannelMetaData;
 import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class ChannelCacheImpl implements ChannelCache
 {
+	private static final Logger logger = LoggerFactory.getLogger(ChannelCacheImpl.class);
 	static private ConcurrentMap<String, ChannelMetaData> channelMetaDataCache = new ConcurrentHashMap<>();
 	private StorageClient storageClient;
 
@@ -34,7 +37,14 @@ public class ChannelCacheImpl implements ChannelCache
 					return null;
 				}
 
-				storageClient.putChannelMetaData(new ChannelMetaData(name, ChannelMetaData.TTL_MONTH, false));
+				try
+				{
+					storageClient.putChannelMetaData(new ChannelMetaData(name, ChannelMetaData.TTL_MONTH, false));
+				}
+				catch (DuplicateChannelException e)
+				{
+					logger.info("Channel {} has already been created.", name);
+				}
 
 				newChannelMetaData = storageClient.getChannelMetaData(name);
 			}
